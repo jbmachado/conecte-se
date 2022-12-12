229eac75681e8c707e3b9e52d7eecbe862880ace
Everton Silva <2019001552@restinga.ifrs.edu.br>
2022-12-12 08:54:18 -0300

    Interceptador

--- front-end/src/app/user/help/auth.interceptor.ts
+++ front-end/src/app/user/help/auth.interceptor.ts
@@ -3,16 +3,34 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HTTP_INTERCEPTORS
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { TokenService } from '../service/token.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  private readonly TOKEN_HEADER_KEY = 'Authorization';
 
-  constructor() {}
+  constructor(private token: TokenService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    let authRequest = request;
+    const token = this.token.getToken();
+
+    if (token != null) {
+      authRequest = request.clone({ headers: request.headers.set(this.TOKEN_HEADER_KEY, 'Bearer ' + token) })
+    }
+
+    return next.handle(authRequest);
   }
 }
+
+export const authInterceptorProviders = [
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }
+]
