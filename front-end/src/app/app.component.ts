7dcf93366de2abfbdb745bd4bdd3a0d692a62358
Everton Silva <2019001552@restinga.ifrs.edu.br>
2022-12-16 08:32:43 -0300

    Menu no cabeçalho

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'front-end';
+  title: string = 'front-end';
+  isMenuOpen: boolean = false;
+
+  onSidenavClick(): void {
+    this.isMenuOpen = false;
+  }
 }
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { FooterBarComponent } from './footer-bar/footer-bar.component';
-
 import { authInterceptorProviders } from './user/help/auth.interceptor';
-import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [AppComponent, TopBarComponent, FooterBarComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -26,6 +25,8 @@ import { MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatMenuModule,
     MatIconModule,
+    MatSidenavModule,
+    MatListModule,
     ReactiveFormsModule
   ],
   providers: [authInterceptorProviders],
