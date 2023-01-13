0374493695865dbe354a8b40d7aae7aa0c6db03b
Everton Silva <2019001552@restinga.ifrs.edu.br>
2023-01-13 11:09:47 -0300

    Carregar todas as oportunidades.

--- front-end/src/app/opportunity/opportunity/opportunity.component.ts
+++ front-end/src/app/opportunity/opportunity/opportunity.component.ts
@@ -3,6 +3,10 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { OpportunityFormDialogComponent } from '../opportunity-form-dialog/opportunity-form-dialog.component';
 import { TokenService } from '../../user/service/token.service';
+import { catchError, Observable, of } from 'rxjs';
+import { Opportunity } from '../model/opportunity';
+import { OpportunityService } from '../service/opportunity.service';
+
 
 @Component({
   selector: 'app-opportunity',
@@ -12,11 +16,13 @@ import { TokenService } from '../../user/service/token.service';
 export class OpportunityComponent implements OnInit {
 
   isLogged: boolean = false;
+  opps$: Observable<Opportunity[]> | null = null;
 
   constructor(
     public dialog: MatDialog,
     public location: Location,
-    private tokenService: TokenService
+    private tokenService: TokenService,
+    private oppService: OpportunityService
   ) { }
 
   ngOnInit(): void {
@@ -24,6 +30,14 @@ export class OpportunityComponent implements OnInit {
       this.isLogged = log;
     })
     this.tokenService.checkIsLogged();
+
+    this.opps$ = this.oppService.findAll()
+      .pipe(
+        catchError(err => {
+          console.log('Erro ao carregar oportunidades. \n' + err);
+          return of([]);
+        })
+      )
   }
 
   addOpportunity() {
--- front-end/src/app/shared/app-material/app-material.module.ts
+++ front-end/src/app/shared/app-material/app-material.module.ts
@@ -18,7 +18,8 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   exports: [
@@ -41,6 +42,8 @@ import { MatNativeDateModule } from '@angular/material/core';
     FlexLayoutModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MatProgressBarModule,
+    MatDividerModule,
   ],
 })
 export class AppMaterialModule {}
